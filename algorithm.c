8f3d48c54368a6e69e95cf1606bdb67d5905b706
AndreaDejanGrande <dejan-grande@mail.com>
2019-02-14 00:04:26 +0000

    vCrypt algorithm names made default

--- algorithm.c
+++ algorithm.c
@@ -18,8 +18,7 @@ void set_algorithm(algorithm_t* algo, const char* newname) {
     algo->name[sizeof(algo->name) - 1] = '\0';
 
     if ((strcmp(algo->name, "adaptive-n-factor") == 0) ||
-	(strcmp(algo->name, "adaptive-nfactor")  == 0) ||
-	(strcmp(algo->name, "vcrypt")           == 0) ) {
+	(strcmp(algo->name, "adaptive-nfactor") == 0) ) {
 	set_algorithm_nfactor(algo, 11);
     } else {
 	set_algorithm_nfactor(algo, 10);
